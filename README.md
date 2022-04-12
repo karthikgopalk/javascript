77c85d16d59b2a9e56723f47793f898c4b93cd82
karthikgopalk <riokarthy@gmail.com>
2022-04-13 01:20:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-m8jmw6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-m8jmw6)
---
+++ script.js
@@ -0,0 +1,64 @@
+// let admin;
+// const NAME = 'jhon';
+// admin = NAME;
+// alert(admin);
+
+// let ourPlanetName = 'Earth'
+// let currentUserName
+
+// alert(1 / 0);
+
+// const infinity = Infinity;
+// console.log(typeof infinity);
+
+// let world = 'world';
+// let demo = `hello ${world}`;
+// console.log(demo);
+
+// function demo() {
+//   'hello';
+// }
+// console.log(typeof demo);
+
+// let arr = [1, 2, 3];
+// console.log(typeof arr);
+
+// alert(`hi ${1}`);
+// alert(`hi ${'hi'}`);
+
+// let promtTest = prompt('enter the text');
+// console.log(promtTest);
+// let trythis = confirm('hi');
+// console.log(trythis);
+
+// let age = prompt('your age');
+// console.log(`your age is ${2022 - age}`);
+
+// let val = NaN;
+// alert(Boolean(val));
+
+// function arg(a, b, c) {
+//   console.log(arguments[0]);
+//   console.log(arguments[1]);
+//   console.log(arguments[2]);
+//   console.log(arguments[3]);
+// }
+// arg(55, 66, 77);
+
+// console.log(5 % 3);
+// console.log(3 ** 3);
+
+// console.log(3 * '3');
+
+// let a = true;
+// console.log(-a);
+
+// let a, b;
+// a = b = 2;
+// console.log(a, b);
+
+// let n = 5;
+// n += 6;
+// console.log(n);
+let a = 5;
+alert(a++);
