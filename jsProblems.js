03631227a67c7d2e148d97d4cb82897756eb8e94
karthikgopalk <riokarthy@gmail.com>
2022-04-15 18:15:09 +0530

    1 file modified

---
+++ jsProblems.js
@@ -0,0 +1,5 @@
+// let input1 = +prompt('enter input 1');
+// let input2 = +prompt('enter input 2');
+// input1 > input2
+//   ? console.log('input 1 is greater')
+//   : console.log('input 2 is greater');
