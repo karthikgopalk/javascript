ec0904c051c10e3ee8d1e5bff6ec6779bb327f85
karthikgopalk <riokarthy@gmail.com>
2022-04-17 11:14:36 +0530

    1 file modified

--- script.js
+++ script.js
@@ -176,12 +176,99 @@
 // let x = txt.slice(10, 17);
 // console.log(x);
 
-for (let i = 1; i <= 10; i++) {
-  if (i % 2 == 0) {
-    console.log(i);
-  }
-}
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
 
 // for (let i = 0; i < 3; i++) {
+//   console.log(`${i}`);
+// }
+// let j = 0;
+// while (j < 3) console.log('while', j++);
+
+// let n = 10;
+// primeNumber: for (let i = 2; i <= n; i++) {
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) continue primeNumber;
+//   }
 //   console.log(i);
 // }
+
+// let num;
+// do {
+//   num = +prompt('enter number greater than 100', 0);
+// } while (num <= 100 && num);
+
+// let a = 2 + 2;
+// switch (a) {
+//   case 3:
+//     console.log('too low');
+//     break;
+//   case 4:
+//     console.log('equal');
+//     break;
+//   case 5:
+//     console.log('high');
+//     break;
+//   default:
+//     console.log('number');
+// }
+
+// let a = +prompt('a?', '');
+
+// if (a == 0) {
+//   alert(0);
+// }
+// if (a == 1) {
+//   alert(1);
+// }
+
+// if (a == 2 || a == 3) {
+//   alert('2,3');
+// }
+
+// let a = +prompt('a?', '');
+// switch (a) {
+//   case 0:
+//     alert(0);
+//     break;
+//   case 1:
+//     alert(1);
+//     break;
+//   case 2 || 3:
+//     alert('2,3');
+//     break;
+//   default:
+//     alert('nothing');
+// }
+
+// function demo(p1, p2 = 'world') {
+//   return console.log(p1, p2);
+// }
+// demo('hi', 'people');
+
+// function checkAgeHandler() {
+//   let age = +prompt('enter the age', 0);
+//   if (age < 18) console.log('minor');
+//   else if (age > 18) console.log('Major');
+//   return;
+// }
+// checkAgeHandler();
+
+// function showCode() {
+//   10;
+// }
+// console.log(showCode);
+
+function ask(question,yes,no){
+if(confirm(question))
+}
+function yes(){
+  console.log('user pressed yes')
+}
+function no(){
+  console.log('user pressed no')
+}
+ask('are you sure to press',yes(),no())
