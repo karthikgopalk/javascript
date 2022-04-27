5f151ab8178e8ea0771bb0cef7ace90ef720885e
karthikgopalk <riokarthy@gmail.com>
2022-04-27 09:17:45 +0530

    1 file modified

---
+++ script.js
@@ -0,0 +1,25 @@
+let billion = 1_000_000_000;
+console.log(billion);
+console.log(7.3e9);
+
+let num = 255;
+console.log(num.toString(16));
+console.log((123456).toString(36));
+
+console.log(Math.floor(3.9));
+console.log(Math.ceil(4.1));
+console.log(Math.round(5.45));
+console.log(Math.trunc(8.98));
+console.log(+(1.67).toFixed(1));
+console.log(+(0.1 + 0.2).toFixed(2));
+console.log(9999999999999999);
+console.log(+0 === -0);
+console.log(isNaN(NaN));
+console.log(isFinite('6.78'));
+console.log(isFinite(76));
+consoles.log(parseInt('67'));
+prompt('hello');
+let a = parseInt(prompt('enter a', 0));
+// let b = Number(prompt('enter b', 0));
+// let c = +prompt('enter c', 0);
+// console.log(a + b + c);
