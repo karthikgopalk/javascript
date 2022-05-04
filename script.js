72fe1ed9a47bb65a0afbc16e32b877f5b621762f
karthikgopalk <riokarthy@gmail.com>
2022-05-04 11:23:42 +0530

    1 file modified

--- script.js
+++ script.js
@@ -17,21 +17,21 @@
 //   console.log(key);
 // }
 
-let range = {
-  from: 5,
-  to: 10,
-};
-range[Symbol.iterator] = function () {
-  return {
-    current: this.from,
-    last: this.to,
-    next() {
-      if (this.current <= this.last) {
-        return { done: false, value: this.current++ };
-      } else return { done: true };
-    },
-  };
-};
-for (let rangeValue of range) {
-  console.log(rangeValue);
-}
+// let range = {
+//   from: 5,
+//   to: 10,
+// };
+// range[Symbol.iterator] = function () {
+//   return {
+//     current: this.from,
+//     last: this.to,
+//     next() {
+//       if (this.current <= this.last) {
+//         return { done: false, value: this.current++ };
+//       } else return { done: true };
+//     },
+//   };
+// };
+// for (let rangeValue of range) {
+//   console.log(rangeValue);
+// }
