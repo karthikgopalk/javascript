0fa00b52bc224302f3686904a1175543a1ef83ff
karthikgopalk <riokarthy@gmail.com>
2022-05-08 12:09:03 +0530

    1 file modified

---
+++ script.js
@@ -0,0 +1,41 @@
+//recursion//
+// function power(x, n) {
+//   return x ** n;
+// }
+// console.log(power(2, 2));
+// console.log(power(2, 3));
+// console.log(power(2, 4));
+
+// function pow(x, n) {
+//   if (n == 1) {
+//     return x;
+//   } else {
+//     return x * pow(x, n - 1);
+//   }
+// }
+// console.log(pow(2, 6));
+
+//factorial//
+
+// function factorial(n) {
+//   return n == 1 || n == 0 ? 1 : n * factorial(n - 1);
+// }
+// console.log(factorial(4));
+// console.log(factorial(6));
+
+//sum of numbers//
+
+// function sumTo(n) {
+//   return n == 1 || n == 0 ? 1 : n + sumTo(n - 1);
+// }
+// console.log(sumTo(5));
+
+// function sumTo(n) {
+//   return (n * (n + 1)) / 2;
+// }
+// console.log(sumTo(5));
+
+// function fib(n) {
+//   return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+// }
+// console.log(fib(3));
