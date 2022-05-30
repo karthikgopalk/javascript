0afc29c19bb9e0981b44301813e0656803bcf9d9
karthikgopalk <riokarthy@gmail.com>
2022-05-30 19:13:21 +0530

    4 files modified

--- jsLearning4.js
+++ jsLearning4.js
@@ -358,3 +358,45 @@
 // console.log(Object.getPrototypeOf(animal) == rabbit);
 // console.log(Object.setPrototypeOf(rabbit, {}));
 // console.log(rabbit);
+
+// class Demo {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   printHello() {
+//     console.log(this.name);
+//   }
+// }
+// let demo = new Demo('karthi');
+// demo.printHello();
+
+// function Demo(name) {
+//   this.name = name;
+// }
+// Demo.prototype.printHello = function () {
+//   console.log(this.name);
+// };
+// let demo = new Demo('karthi');
+// demo.printHello();
+
+// class Demo {
+//   constructor() {
+//     this.name = 'karthi';
+//   }
+// }
+// console.log(typeof Demo);
+// console.log(Demo.prototype.constructor);
+
+// let Demo = class {
+//   constructor() {
+//     this.name = 'hello';
+//   }
+//   get user() {
+//     return this._name;
+//   }
+//   set user(value) {
+//     [this._name] = value;
+//   }
+// };
+// let demo = new Demo();
+// console.log(demo);
