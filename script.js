735e6d2674f08ffb7e8ef57a1d50fca95e029106
karthikgopalk <riokarthy@gmail.com>
2022-06-09 07:25:40 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-// let grandParent = document.querySelector('.grandParent');
-// let parent = document.querySelector('.parent');
-// let child = document.querySelector('.child');
-// grandParent.addEventListener('click', (e) => {
-//   console.log(e.target);
-// });
+// const activityItems = [
+//   {
+//     name: 'Sunday',
+//     items: [
+//       {
+//         name: 'Gym',
+//         activity: 'weights',
+//       },
+//     ],
+//   },
+//   {
+//     name: 'Monday',
+//     items: [
+//       {
+//         name: 'Track',
+//         activity: 'race',
+//       },
+//       {
+//         name: 'Work',
+//         activity: 'meeting',
+//       },
+//       {
+//         name: 'Swim',
+//         items: [
+//           {
+//             name: 'Beach',
+//             activity: 'scuba diving',
+//           },
+//           {
+//             name: 'Pool',
+//             activity: 'back stroke',
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ];
+
+// const output = activityItems.filter((x) =>
+//   x.items.every((y) => y.name.includes('Basketball'))
+// );
