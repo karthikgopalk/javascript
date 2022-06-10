86acf4ad166e92b6e59d9a77af34aafe726f8535
karthikgopalk <riokarthy@gmail.com>
2022-06-10 15:06:20 +0530

    2 files modified, 1 added

---
+++ jsLearning5.js
@@ -0,0 +1,41 @@
+// const activityItems = [
+//   {
+//     name: 'Sunday',
+//     items: [
+//       {
+//         name: 'Gym',
+//         activity: 'weights',
+//       },
+//     ],
+//   },
+//   {
+//     name: 'Monday',
+//     items: [
+//       {
+//         name: 'Track',
+//         activity: 'race',
+//       },
+//       {
+//         name: 'Work',
+//         activity: 'meeting',
+//       },
+//       {
+//         name: 'Swim',
+//         items: [
+//           {
+//             name: 'Beach',
+//             activity: 'scuba diving',
+//           },
+//           {
+//             name: 'Pool',
+//             activity: 'back stroke',
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ];
+
+// const output = activityItems.filter((x) =>
+//   x.items.every((y) => y.name.includes('Basketball'))
+// );
--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-// const activityItems = [
-//   {
-//     name: 'Sunday',
-//     items: [
-//       {
-//         name: 'Gym',
-//         activity: 'weights',
-//       },
-//     ],
-//   },
-//   {
-//     name: 'Monday',
-//     items: [
-//       {
-//         name: 'Track',
-//         activity: 'race',
-//       },
-//       {
-//         name: 'Work',
-//         activity: 'meeting',
-//       },
-//       {
-//         name: 'Swim',
-//         items: [
-//           {
-//             name: 'Beach',
-//             activity: 'scuba diving',
-//           },
-//           {
-//             name: 'Pool',
-//             activity: 'back stroke',
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ];
+// let a = {
+//   name: 'hello',
+// };
+// let b = { ...a };
+// b.name = 'test';
+// console.log(a);
+// console.log(b);
 
-// const output = activityItems.filter((x) =>
-//   x.items.every((y) => y.name.includes('Basketball'))
-// );
+// let a = {
+//   name: 'test',
+//   add: 1,
+//   // add: {
+//   //   code: 1,
+//   // },
+// };
+// let b = { ...a };
+// b.add = 2;
+// console.log(a.add);
+// console.log(b.add);
+
+// let a = 10;
+// let b = a;
+// b = 5;
+// console.log(a);
+// console.log(b);
+
+function square(n) {
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      result += n;
+    }
+  }
+  return result;
+}
+console.log(square(555));
