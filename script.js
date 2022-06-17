e36a3ddd4f83d26287330c80ef50e721c96ac1a3
karthikgopalk <riokarthy@gmail.com>
2022-06-17 16:47:46 +0530

    1 file modified

--- script.js
+++ script.js
@@ -24,13 +24,106 @@
 // console.log(a);
 // console.log(b);
 
-function square(n) {
-  let result = 0;
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= n; j++) {
-      result += n;
-    }
-  }
-  return result;
+// function square(n) {
+//   let result = 0;
+//   for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= n; j++) {
+//       result += n;
+//     }
+//   }
+//   return result;
+// }
+// console.log(square(555));
+
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(() => console.log(i), 1000);
+// }
+
+// const product = (a) => (b) => a * b;
+// console.log(product(4)(5));
+
+// console.log('starts');
+// setTimeout(() => console.log('settimeout'), 0);
+// Promise.resolve()
+//   .then((res) => console.log('promise 1'))
+//   .then((res) => console.log('promise 2'));
+// console.log('end');
+
+// console.log('starts');
+// function demo(sample) {
+//   console.log('demo');
+//   return sample();
+// }
+// function sample() {
+//   console.log('sample');
+// }
+// demo(sample);
+// console.log('end');
+
+// function outer() {
+//   let innerValue = 10;
+//   function inner() {
+//     let b = 'xyz';
+//     console.log(innerValue, b);
+//   }
+//   return inner;
+// }
+// outer()();
+
+// const items = [
+//   { name: 'Bike', price: 100 },
+//   { name: 'TV', price: 200 },
+//   { name: 'Album', price: 10 },
+//   { name: 'book', price: 5 },
+//   { name: 'Phone', price: 500 },
+//   { name: 'Computer', price: 1000 },
+//   { name: 'Keyboard', price: 25 },
+// ];
+
+// const filtered = items.filter((val) => val.price > 10);
+// console.log(filtered);
+
+// const mapped = items.map((val) => val.price + 10);
+// console.log(mapped);
+
+// const forEaches = items.forEach((val) => val.price);
+// console.log('foreach', forEaches);
+
+// const somes = items.some((val) => val.price > 10);
+// console.log(somes);
+
+// const everys = items.every((val) => val.price > 20);
+// console.log(everys);
+
+// // const filled = items.fill(20);
+// // console.log(filled);
+
+// const find = items.find((val) => {
+//   return val.name === 'TV';
+// });
+// console.log(find);
+
+// const reduce = items.reduce((acc, cur) => {
+//   return acc + cur.price;
+// }, 0);
+// console.log(reduce);
+
+// const arr = [1, 2, 3, 4, 5];
+// const includes = arr.includes(1
+//   );
+// console.log(includes);
+
+let obj = {
+  firstName: 'pika',
+  lastName: 'chu',
+  fname: this.firstName,
+  onFullName() {
+    return this.firstName + this.lastName;
+  },
+};
+function getFullName() {
+  return this.onFullName() + ' ' + 'like pokeman';
 }
-console.log(square(555));
+let bindName = getFullName.bind(obj);
+console.log(bindName());
+console.log(obj.fname);
